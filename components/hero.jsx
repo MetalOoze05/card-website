e73d88b4f6a56684b33721177ec8877643547bc1
MetalOoze <38457291+MetalOoze05@users.noreply.github.com>
2022-02-07 15:56:48 +0530

    fixed the socials on mobile and skills

--- components/hero.jsx
+++ components/hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
             <p className="text-gray-500 text-md">Hey there</p>
             <h1 className="text-gray-500 text-4xl">I'm <span className="text-gray-300">MetalOoze</span>, a website developer from India.</h1>
             <div className="flex flex-row items-center mt-7">
-                <div className="flex flex-row flex-wrap justify-center items-center">
+                <div className="flex flex-row  justify-center items-center">
                     <h3 id={styles.heading} className="text-gray-700 text-lg align-top -ml-2">SKILLS</h3>
                     <ul className="ml-3 flex flex-col md:flex-row md:gap-5">
                         <li>
@@ -25,7 +25,7 @@ export default function Hero() {
                 <DiscordStatus />
 
             </div>
-            <div className="absolute bottom-0 left-0 w-full h-2 bg-gradient-to-r from-gray-300 to-gray-500"></div>
+            <div className="fixed bottom-0 left-0 w-full h-2 bg-gradient-to-r from-gray-300 to-gray-500"></div>
             {/*
             <div className="mt-10 flex flex-col items-center gap-2 animate-bounce">
                 <span className="text-gray-400">Scroll Down</span>
