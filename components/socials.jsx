33d6e3aa3aa0746b9e47bd332710df1aa769758a
MetalOoze <38457291+MetalOoze05@users.noreply.github.com>
2022-02-07 15:48:12 +0530

    added social links

--- components/socials.jsx
+++ components/socials.jsx
@@ -3,9 +3,9 @@ export default function Socials() {
         <div className="fixed top-3/4 left-10 md:top-1/2 md:right-10 md:left-auto">
             <div className="flex flex-col items-center">
                 <div className="flex flex-col items-center justify-center gap-5 text-gray-400 text-center md:text-right mb-5">
-                    <a className="fab fa-twitter font-bold hover:text-gray-100 transition duration-200" href=""></a>
-                    <a className="fab fa-github font-bold hover:text-gray-100 transition duration-200" href=""></a>
-                    <a className="fab fa-discord font-bold hover:text-gray-100 transition duration-200" href=""></a>
+                    <a className="fab fa-twitter font-bold hover:text-gray-100 transition duration-200" href="https://twitter.com/MetalOoze_"></a>
+                    <a className="fab fa-github font-bold hover:text-gray-100 transition duration-200" href="https://github.com/MetalOoze05"></a>
+                    <a className="fab fa-discord font-bold hover:text-gray-100 transition duration-200" href="https://discord.gg/AeDJpvXHg4"></a>
                 </div>
                 <div className="w-px h-screen bg-gray-400"></div>
             </div>
