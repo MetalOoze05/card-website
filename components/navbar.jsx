5ff6c200a6df45daa008278636a31d8f6ddcb5fc
MetalOoze <38457291+MetalOoze05@users.noreply.github.com>
2022-02-15 21:41:19 +0530

    added logo

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -22,13 +22,13 @@ export default function Navbar() {
 
     return (
         <nav className='fixed bg-zinc-900 w-full px-10 md:px-10 py-5'>
-            <ReactToolTip />
+            <ReactToolTip className='text-white bg-slate-700' />
             <div className="flex flex-row justify-between items-center">
                 <div>
                     <audio id='audio' hidden loop>
                         <source src='/assets/audio.mp3'/>
                     </audio>
-                    <h1 data-tip="Click for an Easter Egg :D" data-effect="solid" id='name' className='text-gray-300'>MetalOoze</h1>
+                    <Image data-color='#9CA3AF' data-border={true} data-tip="Click for an Easter Egg :D" data-effect="solid" id='name' src="/assets/Logo.svg" width="50px" height="50px" alt="MetalOoze" />
                 </div>
                 <div className="flex flex-row justify-center gap-7">
                     <ul>
--- pages/index.jsx
+++ pages/index.jsx
@@ -10,6 +10,7 @@ export default function Home() {
       <title>MetalOoze</title>
       <meta name="title" content="MetalOoze"></meta>
       <meta name="description" content="Internet Wanderer + Website Developer + Gamer trying to reach bronze in valorant"></meta>
+      <link rel="icon" type='image/x-icon' href="/assets/favicon.svg" />
 
       {/*Open Graph*/}
       <meta property="og:type" content="website"></meta>
