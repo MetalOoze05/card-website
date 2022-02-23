bf1a17d92930c93748e0aef64e4a9bfe2a0941b7
MetalOoze <38457291+MetalOoze05@users.noreply.github.com>
2022-02-23 09:54:41 +0530

    removed easter egg

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,34 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { useEffect } from 'react'
-import ReactToolTip from "react-tooltip";
 
 export default function Navbar() {
 
-    useEffect(() => {
-        const audio = document.getElementById('audio')
-        const name = document.getElementById('name')
-        let isPlaying = false
-        name.addEventListener('click', () => {
-            if (!isPlaying) {
-                audio.play()
-                isPlaying = true
-            } else {
-                isPlaying = false
-                audio.pause()
-            }
-        })
-    }, [])
-
     return (
         <nav className='fixed bg-zinc-900 w-full px-10 md:px-10 py-5'>
-            <ReactToolTip className='text-white bg-slate-700' />
             <div className="flex flex-row justify-between items-center">
                 <div>
                     <audio id='audio' hidden loop>
                         <source src='/assets/audio.mp3'/>
                     </audio>
-                    <Image data-color='#9CA3AF' data-border={true} data-tip="Click for an Easter Egg :D" data-effect="solid" id='name' src="/assets/Logo.svg" width="50px" height="50px" alt="MetalOoze" />
+                    <Image id='name' src="/assets/Logo.svg" width="50px" height="50px" alt="MetalOoze" />
                 </div>
                 <div className="flex flex-row justify-center gap-7">
                     <ul>
